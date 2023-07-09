45965af8704583f062a9f5fb25a05968d4a2b795
JosefKru <josefkaru@gmail.com>
2023-07-09 14:23:26 +0300

    add react-router-dom

--- config/build/buildDevServer.ts
+++ config/build/buildDevServer.ts
@@ -6,5 +6,6 @@ export function buildDevServer(options: BuildOptions): Configuration {
   return {
     open: true,
     port,
+    historyApiFallback: true,
   }
 }
--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,27 @@
+import { BrowserRouter, Route, Routes, Link } from 'react-router-dom'
 import Counter from './components/Counter'
 import './index.scss'
+import About from './pages/AboutPage/AboutPage'
+import Main from './pages/MainPage/MainPage'
+import { MainPageAsync } from './pages/MainPage/MainPage.async'
+import { AboutPageAsync } from './pages/AboutPage/AboutPage.async'
+import { Suspense } from 'react'
 
 const App = () => {
   return (
-    <div className='app'>
-      "Это текст текст текст "
-      <Counter />
-    </div>
+    <BrowserRouter>
+      <div className='app'>
+        <Link to='/'>Главная</Link>
+        <Link to='/about'>О нас</Link>
+        <Suspense fallback={<div>Loading...</div>}>
+          <Routes>
+            <Route path='/' element={<MainPageAsync />} />
+            <Route path='/about' element={<AboutPageAsync />} />
+          </Routes>
+        </Suspense>
+        {/* <Counter /> */}
+      </div>
+    </BrowserRouter>
   )
 }
 
