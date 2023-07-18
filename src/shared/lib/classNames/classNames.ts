1a7307557c788c4b19758a3173f9d333ac69ca42
JosefKru <josefkaru@gmail.com>
2023-07-18 18:30:06 +0300

    add jest, fix classNames

--- src/shared/lib/classNames/classNames.ts
+++ src/shared/lib/classNames/classNames.ts
@@ -1,15 +1,13 @@
-type Modes = Record<string, boolean | string>
-
 export function classNames(
   cls: string,
-  mods?: Modes,
-  optional?: string[]
+  mods: Modes = {},
+  optional: string[] = []
 ): string {
   return [
     cls,
-    ...optional.filter(Boolean),
     ...Object.entries(mods)
       .filter(([_, value]) => Boolean(value))
       .map(([cls, _]) => cls),
+    ...optional.filter(Boolean),
   ].join(' ')
 }
